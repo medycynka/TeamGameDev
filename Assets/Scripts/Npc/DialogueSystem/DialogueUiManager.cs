4d8bed8f88a304674791791b3bfb8f847c6140f1
medycynka <tyrranozaur08@gmail.com>
2021-05-30 21:28:53 +0200

    hot fixes time - fixed dialogue quest giving text

--- Assets/Scripts/Npc/DialogueSystem/DialogueUiManager.cs
+++ Assets/Scripts/Npc/DialogueSystem/DialogueUiManager.cs
@@ -111,7 +111,6 @@ namespace SzymonPeszek.Npc.DialogueSystem
                 
                 if (_npcInteractionManager.dialogueMap[targetGuid].giver && !_npcInteractionManager.isQuestGiven)
                 {
-                    mainText.text = _npcManager.currentMainQuest.quest.questTaskText;
                     options[0].optionObject.SetActive(true);
                     options[0].button.onClick.AddListener(GiveQuest);
                     options[0].optionText.text = _npcInteractionManager.dialogueMap[targetGuid].links.Count > 0
