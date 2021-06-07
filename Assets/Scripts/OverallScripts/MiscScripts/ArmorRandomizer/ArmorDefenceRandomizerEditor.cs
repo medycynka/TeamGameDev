a70f561023a66782aa0253b761de166580b738e3
medycynka <tyrranozaur08@gmail.com>
2021-06-07 10:04:18 +0200

    hot fixes time - randomize armor values and added shrines

---
+++ Assets/Scripts/OverallScripts/MiscScripts/ArmorRandomizer/ArmorDefenceRandomizerEditor.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace SzymonPeszek.Misc
+{
+    [CustomEditor(typeof(ArmorDefenceRandomizer))]
+    public class ArmorDefenceRandomizerEditor: Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            ArmorDefenceRandomizer targetScript = (ArmorDefenceRandomizer) target;
+
+            if (GUILayout.Button("Randomize armor"))
+            {
+                DateTime start = DateTime.Now;
+                targetScript.RandomizeArmorValues();
+                DateTime stop = DateTime.Now;
+                    
+                Debug.Log("Took " + (stop - start).Milliseconds + " ms");
+            }
+        }
+    }
+}
+#endif
