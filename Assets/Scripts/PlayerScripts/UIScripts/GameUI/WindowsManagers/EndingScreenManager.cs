09b69f230a641465fc629559896103a93b8fe75a
medycynka <tyrranozaur08@gmail.com>
2021-06-11 14:57:03 +0200

    tidy changes

--- Assets/Scripts/PlayerScripts/UIScripts/GameUI/WindowsManagers/EndingScreenManager.cs
+++ Assets/Scripts/PlayerScripts/UIScripts/GameUI/WindowsManagers/EndingScreenManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,6 +13,19 @@ namespace SzymonPeszek.GameUI.WindowsManagers
 
         public void BackToMainMenu()
         {
+            if (Directory.Exists($"{Application.dataPath}/Saves"))
+            {
+                string[] tmp = Directory.GetFiles($"{Application.dataPath}/Saves");
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    File.Delete(tmp[i]);
+                }
+
+                if (!Directory.Exists($"{Application.dataPath}/Saves"))
+                {
+                    Directory.CreateDirectory($"{Application.dataPath}/Saves");
+                }
+            }
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
